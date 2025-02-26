76e0771bf62b3bbb6a8b0fcf6b27856a6ef55b5a
OnlyHans1 <adityarhaihan@gmail.com>
2025-02-26 22:52:51 +0700

    feat: order form

--- app/Repositories/Contracts/OrderRepositoryInterface.php
+++ app/Repositories/Contracts/OrderRepositoryInterface.php
@@ -5,7 +5,7 @@ namespace App\Repositories\Contracts;
 interface OrderRepositoryInterface
 {
     public function createTransaction(array $data);
-    public function findByTrxAndPhoneNumber($bookingTrxId, $phoneNumber);
+    public function findByTrxIdAndPhoneNumber($bookingTrxId, $phoneNumber);
     public function saveToSession(array $data);
     public function updateSessionData(array $data);
     public function getOrderDataFromSession();
